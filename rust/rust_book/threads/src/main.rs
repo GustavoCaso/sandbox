9aac0f83c9db172048b07266032f010decd3f8a5
Gustavo Caso <gustavocaso@gmail.com>
2025-09-08 20:24:42 +0200

    concurrency chapter

---
+++ rust/rust_book/threads/src/main.rs
@@ -0,0 +1,88 @@
+use std::sync::{Arc, Mutex, mpsc};
+use std::thread;
+use std::time::Duration;
+
+fn threads() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || println!("Here's the vector: {v:?}"));
+
+    handle.join().unwrap()
+}
+
+fn channels() {
+    // mpsc stand for multiple produceer, single consumer
+    let (tx, rx) = mpsc::channel();
+    // let tx1 = tx.clone();
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("hi"),
+    //         String::from("from"),
+    //         String::from("the"),
+    //         String::from("thread"),
+    //     ];
+
+    //     for val in vals {
+    //         tx1.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("meesages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+}
+
+fn mutex() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Results: {}", *counter.lock().unwrap());
+}
+
+fn main() {
+    threads();
+    channels();
+    mutex();
+}
